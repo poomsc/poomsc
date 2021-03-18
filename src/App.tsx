6b0352d89e883dfa97329638e8835aa4428cb457
poomsc <poom.genius@gmail.com>
2021-03-18 13:48:52 +0700

    change main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,36 @@
-import React, { useContext } from "react";
+import React from "react";
 import "./App.css";
-import ConnectWallet from "components/ConnectWallet";
-import Sidebar from "components/Sidebar";
-import { SidebarContext } from "context/sidebarContext";
-import Main from "container/Main";
-import Navbar from "components/Navbar";
 
 function App() {
-  const { isSidebarOpen } = useContext(SidebarContext);
-  return (
-    <div
-      className={`flex h-screen bg-secondary dark:bg-gray-900 ${
-        isSidebarOpen && "overflow-hidden"
-      }`}
-    >
-      {/* <Sidebar /> */}
-      <div className="flex flex-1 flex-col w-full">
-        <Navbar />
-        <Main>
-          Hello world
-        </Main>
-      </div>
-    </div>
-  );
+  return <div>Hello world</div>;
 }
 
 export default App;
+// import React, { useContext } from "react";
+// import "./App.css";
+// import ConnectWallet from "components/ConnectWallet";
+// import Sidebar from "components/Sidebar";
+// import { SidebarContext } from "context/sidebarContext";
+// import Main from "container/Main";
+// import Navbar from "components/Navbar";
+
+// function App() {
+//   const { isSidebarOpen } = useContext(SidebarContext);
+//   return (
+//     <div
+//       className={`flex h-screen bg-secondary dark:bg-gray-900 ${
+//         isSidebarOpen && "overflow-hidden"
+//       }`}
+//     >
+//       {/* <Sidebar /> */}
+//       <div className="flex flex-1 flex-col w-full">
+//         <Navbar />
+//         <Main>
+//           Hello world
+//         </Main>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
